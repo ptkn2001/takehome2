210d8612c3cde17a6f00198089732f10f6a5c3bf
Derrick Nguyen <ptkn2001@msn.com>
2022-02-28 00:37:46 -0800

    use build for production

--- server.js
+++ server.js
@@ -9,13 +9,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client', 'build')));
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-// });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
 
 app.use(routes);
 
